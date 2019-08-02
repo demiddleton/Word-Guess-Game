4053705707e49dba32426e17d8018f4fb31e5ddf
Denise Middleton <demiddleton@bellsouth.net>
2019-08-01 21:12:09 -0400

    created pseudocode for game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Create an array of songs
+//Choose songs randomly
+//Create dasshes based on length of song title
+//Get users guess
+//Check if guess is right
+//If right, push to the right array
+//Continue until all letters are guessed or user runs out of guesses
+//If wrong guess push to wrong array
+//If user guesses all letters, display the matching image
+//If user does not guess song, display word you lose.
